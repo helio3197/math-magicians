1eceff4f2e99ec950f6dd08bf7ea83ec8e868a54
Ntare Guy <gntare2@gmail.com>
2022-03-23 19:44:29 +0200

    test to divide number

--- src/logic/__tests__/operate.test.js
+++ src/logic/__tests__/operate.test.js
@@ -4,4 +4,7 @@ describe('Tests for Operate Function', () => {
   test('Test adding 1 + 2 equal to 3', () => {
     expect(operate('1', '2', '+')).toBe('3');
   });
+  test('Test dividing 4 by 2 to be 2 ', () => {
+    expect(operate('4', '2', '÷')).toBe('2');
+  });
 });

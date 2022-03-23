f37017f248879655121ef1d95c1cd291667786b6
Ntare Guy <gntare2@gmail.com>
2022-03-23 19:45:55 +0200

    test to substract number

--- src/logic/__tests__/operate.test.js
+++ src/logic/__tests__/operate.test.js
@@ -7,4 +7,7 @@ describe('Tests for Operate Function', () => {
   test('Test dividing 4 by 2 to be 2 ', () => {
     expect(operate('4', '2', '÷')).toBe('2');
   });
+  test('Test Substract 6 by 2 to be 4 ', () => {
+    expect(operate('6', '2', '-')).toBe('4');
+  });
 });
